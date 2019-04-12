83053b722854b962e84d03894bb17a63a50a207f
FxBresson <fx-bresson@live.fr>
2019-04-12 15:36:50 +0200

    @Add: GraphQL queries

---
+++ helpers/queries.js
@@ -0,0 +1,34 @@
+export const getLineup = `
+query getLineup($id: MongoID!) {
+  lineupById(_id: $id) {
+    _id
+    name
+    description
+    objectives
+    players {
+      mainBtag
+    }
+    strats {
+      _id
+    }
+    matchHistory {
+      date
+    }
+    matchSchedule {
+      date
+    }
+  }
+}
+`
+
+export const getPlayer = `
+query GetPlayer($player: FilterFindOnePlayerInput) {
+  playerOne(filter: $player) {
+    _id
+    mainBtag
+    lineup
+    status
+    role
+  }
+}
+`
