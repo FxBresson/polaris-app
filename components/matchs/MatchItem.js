a66760f365d604f6299c4a56e7c2782a4e7f9d3b
FxBresson <fx-bresson@live.fr>
2019-04-12 15:32:35 +0200

    @Add: Match components

---
+++ components/matchs/MatchItem.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
+
+export default class MatchItem extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          this.props.navigation.navigate('Match', {
+            matchId: this.props._id,
+          });
+        }}
+      >
+        <View>
+          <View>
+            <Text>{this.props.date}</Text>
+            {this.props.sr &&
+              <Text>{this.props.sr}</Text>
+            }
+          </View>
+          <View>
+            <Text>{this.props.type}</Text>
+            {this.props.score &&
+              <Text>{this.props.score}</Text>
+            }
+          </View>
+        </View>
+        <View></View>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
