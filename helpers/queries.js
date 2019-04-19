1cacdb78081366ed07e3fc4762722994830adf11
FxBresson <fx-bresson@live.fr>
2019-04-19 17:54:31 +0200

    #Edit: Add queries

--- helpers/queries.js
+++ helpers/queries.js
@@ -1,5 +1,27 @@
+const stratObj = `
+_id
+map
+comments
+comps {
+  name
+  isDefense
+  characters
+}
+`
+
+const playerObj = `
+_id
+mainBtag
+lineup
+status
+role {
+name
+}
+doodle
+`
+
 export const getLineup = `
-query getLineup($id: MongoID!) {
+query getLineUp($id: MongoID!) {
   lineupById(_id: $id) {
     _id
     name
@@ -7,9 +29,10 @@ query getLineup($id: MongoID!) {
     objectives
     players {
       mainBtag
+      doodle
     }
     strats {
-      _id
+${stratObj}
     }
     matchHistory {
       date
@@ -22,13 +45,43 @@ query getLineup($id: MongoID!) {
 `
 
 export const getPlayer = `
-query GetPlayer($player: FilterFindOnePlayerInput!) {
+query getPlayer($player: FilterFindOnePlayerInput!) {
   playerOne(filter: $player) {
+${playerObj}
+  }
+}
+`
+
+export const updatePlayer = `
+mutation modifyPlayer($record: UpdateByIdPlayerInput!) {
+  playerUpdateById(record: $record) {
+    record {
+${playerObj}
+    }
+  }
+}
+`
+
+
+
+export const getMaps = `
+query getMaps {
+  mapsMany {
     _id
-    mainBtag
-    lineup
-    status
-    role
+    name
+    mapTypes
+    thumbnail
+    flagUrl
+  }
+}
+`
+
+export const addStrat = `
+mutation addStrat($record: CreateOneStratInput!) {
+  stratCreateOne(record: $record) {
+    record {
+${stratObj}
+    }
   }
 }
 `
