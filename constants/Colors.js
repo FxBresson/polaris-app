e2c1f213fa7da7e161752ab7900db2be01dbe697
FxBresson <fx-bresson@live.fr>
2019-04-28 10:41:31 +0200

    @Add: Polaris colors in constant/Colors.js

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,18 @@
-const tintColor = '#2f95dc';
-
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  navyBlue: '#000066',
+  opacityNavyBlue: 'rgba(0, 0, 102, 0.5)',
+  vividSky: '#00CCFF',
+  denimBlue: '#333399',
+  brandeisBlue: '#0066FF',
+  robinEgg: '#00CCCC',
+
+  blueGradient:{
+    from: 'rgba(0, 0, 102, 0.7)',
+    to: 'rgba(0, 204, 255, 0.7)'
+  },
+
+  textColor: '#FFFFFF',
+  
+  checkboxYes: '#00DD16',
+  checkboxMaybe: '#EDD500',
 };
