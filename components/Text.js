e2051e83dc4bf2323bdb0b0120ff8f920cd758f6
FxBresson <fx-bresson@live.fr>
2019-04-28 10:42:03 +0200

    @Add: App custom Text Component

---
+++ components/Text.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet
+} from 'react-native';
+import Colors from '../constants/Colors';
+
+const CustomText = (props) => {
+
+  const sizes = {
+    h1: 24,
+    h2: 16,
+    regular: 14
+  }
+
+  let fontSize = sizes.regular
+
+  for (const key in props) {
+    if (props.hasOwnProperty(key) && props[key] && sizes[key]) {
+      fontSize = sizes[key]
+    }
+  }
+
+
+  const styles = StyleSheet.create({
+    text: {
+      color: Colors.textColor,
+      fontSize: fontSize,
+      fontWeight: props.bold ? 'bold' : 'normal',
+      fontStyle: props.italic ? 'italic' : 'normal',
+      ...props.style
+    }
+  })
+
+  return (
+    <Text {...props} style={styles.text}>
+      {props.children}
+    </Text>
+  )
+}
+
+export default CustomText
