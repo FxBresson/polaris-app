96d2a69364556154189a12b9b5f98dc514489d15
FxBresson <fx-bresson@live.fr>
2019-05-08 18:45:45 +0200

    @Edit: Fix Login bug on iOS

--- screens/auth/AuthLoadingScreen.js
+++ screens/auth/AuthLoadingScreen.js
@@ -52,6 +52,7 @@ class AuthLoadingScreen extends React.Component {
 
     //LOAD TOKEN
     // await AsyncStorage.setItem('userToken', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InN0YXR1cyI6WyJQbGF5ZXIiLCJGb3VuZGVyIiwiQ2FwdGFpbiJdLCJfaWQiOiI1Y2FmNDFlNDI3ODg1MGFlNzZhNjFkNjEiLCJtYWluQnRhZyI6IlRvb3RobGVzcyMyMjM2IiwibGluZXVwIjoiNWNhZjQxNDA3NTllNzdkY2RmNDc0YmMxIiwiX192IjowLCJibmV0UHJvZmlsZUlkIjoiMTM4NDAzMTc3In0sImlhdCI6MTU1NTE2MDYwNX0.InnlY95Pkq7A_gqCLUnuloXoxYhtV0cbloT4t2wwP70')
+    // await AsyncStorage.removeItem('userToken');
     const userToken = await AsyncStorage.getItem('userToken');
     if (userToken) {
       this.logPlayerIn(userToken, false)
@@ -67,7 +68,12 @@ class AuthLoadingScreen extends React.Component {
     let redirectUrl = await Linking.getInitialURL()
     let authUrl = `https://polarisapi.serveo.net/auth/bnet?url=${encodeURIComponent(redirectUrl)}`
     try {
-      await WebBrowser.openAuthSessionAsync(authUrl)
+      const loginState = await WebBrowser.openAuthSessionAsync(authUrl, redirectUrl)
+
+      if (loginState.type === 'success') {
+        this.handleRedirect(loginState)
+      }
+      
     } catch (err) {
       console.error(err)
     }
@@ -88,13 +94,13 @@ class AuthLoadingScreen extends React.Component {
   async logPlayerIn(token, newLogin) {
     try {
       await this.props.global.login(token, newLogin)
-      this.loadDataAsync(newLogin)
+      await this.loadDataAsync(newLogin)
     } catch(err) {
       this.setState({loginError: err})
     }
   }
 
-  async loadDataAsync(token, newLogin) {
+  async loadDataAsync(newLogin) {
     try {
       await this.setState({ loginError: null, needLogin: false, isNewLogin: newLogin, isLoadingData: true })
       await this.props.global.requester(GET_LINEUP)
@@ -132,7 +138,7 @@ class AuthLoadingScreen extends React.Component {
         }
         
         {this.state.needLogin &&
-          <Button onPress={() => this.handleOAuthLogin()}>
+          <Button style={{marginTop: 40}} onPress={() => this.handleOAuthLogin()}>
             <Text>Login</Text>
           </Button>
         }
